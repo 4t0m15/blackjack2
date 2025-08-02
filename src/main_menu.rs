5731c425313f807fdcbc9007707afb7b8218649c
Arsen Martirosyan <arsenmartirosyan@protonmail.com>
2025-08-02 05:05:52 -0700

    Reformat main_menu.rs for consistent indentation

--- src/main_menu.rs
+++ src/main_menu.rs
@@ -1,32 +1,32 @@
-    pub struct PrintAbout;
-    impl PrintAbout {
-        pub fn doit(&self) {
-            println!(
-                "\
+pub struct PrintAbout;
+impl PrintAbout {
+    pub fn doit(&self) {
+        println!(
+            "\
             About: A simple text-based blackjack game written in Rust.
             Work is CC0 \
             If you paid for it, you should request a refund.\
             Source code is found at: (https://github.com/4t0m15/blackjack2).\
             Created by Arsen Martirosyan.\
             "
-            );
-        }
+        );
     }
-    pub struct PrintHelp;
-    impl PrintHelp {
-        pub fn show_controls(&self) {
-            print!(
-                "\
+}
+pub struct PrintHelp;
+impl PrintHelp {
+    pub fn show_controls(&self) {
+        print!(
+            "\
         Controls: \
             n - Game Start\
             a - About\
             h - Help (where you are now)\
             q - Quit"
-            );
-        }
-        pub fn show_instructions(&self) {
-            print!(
-                " How to play: \
+        );
+    }
+    pub fn show_instructions(&self) {
+        print!(
+            " How to play: \
             THE PLAYER is served a card\
             by a DEALER.\
             THE PLAYER can choose to HIT\
@@ -38,6 +38,6 @@
             The DEALER will\
             also be served a card.\
             The idea is to have more than the DEALER, but not BUST."
-            );
-        }
+        );
     }
+}
