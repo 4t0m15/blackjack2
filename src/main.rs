874bcfda3c6208b21496bd0e48b69ca3b70df99e
Arsen Martirosyan <arsenmartirosyan@protonmail.com>
2025-08-06 02:30:23 -0700

    get rid of useless error handling i only write the best code ever anyway lol

--- src/main.rs
+++ src/main.rs
@@ -17,51 +17,8 @@ pub mod text_handler;
 pub mod win_checker;
 
 use game_manager::GameManager;
-use std::io::{self, Write};
-use std::panic;
-use std::process;
 
 fn main() {
-    setup_panic_handler();
-
-    match setup_and_run_game() {
-        Ok(_) => {}
-        Err(_) => {
-            process::exit(1);
-        }
-    }
-}
-
-fn setup_and_run_game() -> Result<(), String> {
     let mut game_manager = GameManager::new();
-
     game_manager.run();
-
-    Ok(())
-}
-
-fn setup_panic_handler() {
-    panic::set_hook(Box::new(|panic_info| {
-        eprintln!("\n\n====== GAME CRASHED ======");
-        eprintln!("sorry!");
-
-        if let Some(location) = panic_info.location() {
-            eprintln!("Location: {}:{}", location.file(), location.line());
-        }
-
-        if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
-            eprintln!("Error: {}", s);
-        } else {
-            eprintln!("An unknown error occurred");
-        }
-        eprintln!(
-            "this is a pretty serious error, report it to me @ arsenmartirosyan@protonmail.com"
-        );
-        eprintln!("Press Enter to exit...");
-        let mut buffer = String::new();
-        io::stdout().flush().expect("Failed to flush stdout");
-        let _ = io::stdin().read_line(&mut buffer);
-
-        process::exit(1);
-    }));
 }
