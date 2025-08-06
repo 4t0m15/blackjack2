d48af260a2b7e7572750776c2a67ad35f0bd00cd
Arsen Martirosyan <arsenmartirosyan@protonmail.com>
2025-08-06 02:34:40 -0700

    this should fix github actions setup

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ pub mod art_handler;
 pub mod card_handler;
 pub mod deck_manager;
 pub mod enemy_ai_handler;
+pub mod game_data;
 pub mod game_history;
 pub mod game_history_core;
 pub mod game_history_display;
@@ -13,6 +14,7 @@ pub mod history_menu;
 pub mod main_menu;
 pub mod menu_handling;
 pub mod player_handler;
+pub mod save_system;
 pub mod text_handler;
 pub mod win_checker;
 
